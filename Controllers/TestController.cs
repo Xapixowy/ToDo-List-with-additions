6b4d79bd4318472320a27513855cbff024bd1a70
Jakub Chodziński <xapixowy@gmail.com>
2022-11-11 19:07:27 +0100

    Try to connect to database

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo_List_with_additions.Services;
+
+namespace ToDo_List_with_additions.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async void GetData()
+        {
+            var test = new Mongo();
+            test.MainAsync();
+        }
+    }
+}
--- Services/test.cs
+++ Services/test.cs
@@ -8,30 +8,42 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Xml.Xsl;
 
-class MongoDB
+namespace ToDo_List_with_additions.Services
 {
-    static async Task MainAsync()
+    class Mongo
     {
-        var connectionString = "mongodb+srv://cluster0.btywazj.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
-        var settings = MongoClientSettings.FromConnectionString(connectionString);
-        settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-
-        // You will need to convert your Atlas-provided PEM containing the cert/private keys into a PFX
-        // use openssl and the following line to create a PFX from your PEM:
-        // openssl pkcs12 -export -in <x509>.pem -inkey <x509>.pem -out <x509>.pfx
-        // and provide a password, which should match the second argument you pass to X509Certificate2
-        var cert = new X509Certificate2("<path_to_pfx>", "<pfx_passphrase>");
-
-        settings.SslSettings = new SslSettings
+        public async Task MainAsync()
         {
-            ClientCertificates = new List<X509Certificate>() { cert }
-        };
+            var connectionString = "mongodb+srv://cluster0.btywazj.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
+            var settings = MongoClientSettings.FromConnectionString(connectionString);
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
 
-        var client = new MongoClient(settings);
-        var database = client.GetDatabase("testDB");
-        var collection = database.GetCollection<BsonDocument>("testCol");
+            // You will need to convert your Atlas-provided PEM containing the cert/private keys into a PFX
+            // use openssl and the following line to create a PFX from your PEM:
+            // openssl pkcs12 -export -in <x509>.pem -inkey <x509>.pem -out <x509>.pfx
+            // and provide a password, which should match the second argument you pass to X509Certificate2
+            var cert = new X509Certificate2("./Certificates/Default.perm");
 
-        var docCount = collection.CountDocuments("{}");
-        Console.WriteLine(docCount);
+            settings.SslSettings = new SslSettings
+            {
+                ClientCertificates = new List<X509Certificate>() { cert }
+            };
+
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase("sample_mflix");
+            var collection = database.GetCollection<BsonDocument>("users");
+
+            var document = new BsonDocument
+            {
+                { "item", "canvas" },
+                { "qty", 100 },
+                { "tags", new BsonArray { "cotton" } },
+                { "size", new BsonDocument { { "h", 28 }, { "w", 35.5 }, { "uom", "cm" } } }
+            };
+            collection.InsertOne(document);
+
+            var docCount = collection.CountDocuments("{}");
+            Console.WriteLine(docCount);
+        }
     }
 }
